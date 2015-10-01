921ba5f5e7bab5e8c722fb470b4128b28190830a
Bobby Cafazzo <bcafazzo@nbi.company>
2015-10-01 12:43:03 -0400

    [WCTK-1050] Created an extension to do toolkit links.

---
+++ src/Orchard.Web/Modules/ToolkitPath/ToolkitPathExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace ToolkitPath
+{
+    public static class ToolkitPathExtensions
+    {
+        public static string ToolkitPath(this HtmlHelper htmlHelper, string path)
+        {
+            return ConfigurationManager.AppSettings["ToolkitBrowseUrl"] + path;
+        }
+    }
+}
