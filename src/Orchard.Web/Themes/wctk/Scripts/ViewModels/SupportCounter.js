a328764b9f7082bcf14ba857170b1302acf63b16
Bobby Cafazzo <bcafazzo@nbi.company>
2015-10-14 13:29:28 -0400

    [WCTK-1119] Adding support counter links to cms.

---
+++ src/Orchard.Web/Themes/wctk/Scripts/ViewModels/SupportCounter.js
@@ -0,0 +1,30 @@
+﻿"use strict"
+
+var SupportCounterViewModel = function () {
+    var self = this;
+
+    self.target = $('#ticket-counter').attr('target');
+
+    self.newMessages = ko.observable('?');
+
+    self.update = function () {
+        $.ajax({
+            url: self.target,
+            jsonp: 'callback',
+            dataType: 'jsonp',
+            success: function (results) {
+                self.newMessages(results.unread);
+            }
+        });
+    };
+
+    setTimeout(self.update(), 30000);
+    self.update();
+};
+
+$(document).ready(function () {
+    var area = $('#ticket-counter')[0];
+    if (area) {
+        ko.applyBindings(new SupportCounterViewModel(), area);
+    }
+});
